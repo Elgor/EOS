9ce24ef6cf4ddd9e9e035b166a33815683d60eff
YohanesKiolol <yohakiolol11@gmail.com>
2021-01-15 20:27:39 +0700

    fix

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -30,7 +30,9 @@
                 <td>{{ $transaction->invoice }}</td>
                 <td>{{ $transaction->bank }}</td>
                 <td>{{ $transaction->type }}</td>
-                <td>Rp {{number_format($transaction->order->negotiation_price??$transaction->order->product->price,0,',','.')}}​​</td>
+                <td>Rp
+                    {{number_format($transaction->order->negotiation_price??$transaction->order->product->price,0,',','.')}}​​
+                </td>
             </tr>
             @endforeach
         </tbody>
